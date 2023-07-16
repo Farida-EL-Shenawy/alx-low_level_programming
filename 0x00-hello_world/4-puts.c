5ccb33c184eee299a07eccc98af5b84be9ac5e71
farida <farida@gmail.com>
2023-07-15 23:28:02 -0700

    Edited program puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 /**
- * main is  entry program 
- * return :0 (means succes)
- * */
-int main(){
-	puts("\"Programming is like building a multilingual puzzle");
-         return (0);
+ * main : -A program that prints a line with puts function
+ * return : 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

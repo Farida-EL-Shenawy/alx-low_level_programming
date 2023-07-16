ba6956734b0e4e4a73aaf213f7225c4e8dfb3cee
farida <farida@gmail.com>
2023-07-15 23:22:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * main is  entry program 
+ * return :0 (means succes)
+ * */
+int main(){
+	puts("\"Programming is like building a multilingual puzzle");
+         return (0);
+}

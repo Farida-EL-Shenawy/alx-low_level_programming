250d797b5f13c08a31977ff29b7e218fc8b0f36b
farida <farida@gmail.com>
2023-07-17 16:33:53 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - A program that prints two two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num1 = 0;
+int num2;
+while (num1 <= 99)
+{
+num1 = num2;
+while (num2 <= 99)
+{
+if (num1 != num2)
+{
+putchar((num1 / 10) + 48);
+putchar((num1 % 10) + 48);
+putchar(' ');
+putchar((num2 / 10) + 48);
+putchar((num2 % 10) + 48);
+if (num1 + num2 != 197)
+{
+putchar(',');
+putchar(' ');
+}
+}
+num2++;
+}
+num1++;
+}
+putchar('\n');
+return (0);
+}

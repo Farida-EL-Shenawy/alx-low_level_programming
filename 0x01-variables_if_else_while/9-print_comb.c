58dd4cc97d31dc3d71b719e21b5fddb528068eb7
farida <farida@gmail.com>
2023-07-17 14:12:11 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - A program that prints all single digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+putchar(num + '0');
+if (num <= 9)
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}

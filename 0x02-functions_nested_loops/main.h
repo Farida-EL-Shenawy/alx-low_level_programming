f0b8b831a809e1896108dc4afc85a013e1b22492
farida <farida@gmail.com>
2023-07-18 12:32:34 -0700

    2-print_alphabet_x10.c 101

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include<srdio.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,4 +15,4 @@ void times_tablet(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
+#endif

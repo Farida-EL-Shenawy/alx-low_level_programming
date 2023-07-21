78a53e8c4977e0a5fd2be615d95dfc9c863ba3f8
farida <farida@gmail.com>
2023-07-21 09:50:06 -0700

    11coding

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/**
+ * print_numbers - check the code
+ *
+ * Return: void.
+ */
+
+void print_numbers(void)
+{
+int num;
+
+for (num = 0; num <= 9; num++)
+{
+_putchar(num + '0');
+}
+_putchar('\n');

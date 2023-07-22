9dc892f6498b77375c848c6e0b9894fd586939f7
farida <farida@gmail.com>
2023-07-21 18:21:02 -0700

    gsgf

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_line - a function that draws a straight line
+ *
+ * @n: input
+ *
+ * Return: Always 0 
+ */
+
+void print_line(int n)
+{       
+
+int i;
+
+if (n == 0)
+{
+_puhchar('\n');
+}
+else
+{
+for (i =0; i < n; i++)
+{
+_putchar('_');
+}
+_puhchar('\n');
+}
+}

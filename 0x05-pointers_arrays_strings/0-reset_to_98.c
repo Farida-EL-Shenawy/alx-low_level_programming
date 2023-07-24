11f1a894c184c7f6d4db4bb5ca0bb244ef1e2f7e
farida <farida@gmail.com>
2023-07-24 01:32:00 -0700

    a

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,17 +3,12 @@
 /**
  * reset_to_98 - function updates the value it points to to 98.
  *
- * *n: the pointer
+ * n: the pointer
  *
  * Return: void
  */
 
 void reset_to_98(int *n)
 {
-
-int var = 20;
-int *n;
-
-n = &var;
 *n = 98;
 }

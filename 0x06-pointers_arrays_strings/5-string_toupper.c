5f680b3c360deeaee8f55305c2f5bec2de1ad68b
farida <farida@gmail.com>
2023-07-28 07:52:28 -0700

    sadf

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+/**
+ * *string_toupper - changes lowercase letters to uppercase.
+ *
+ * @str: string to return
+ * 
+ * Return: str
+ */
+
+char *string_toupper(char *str)
+{
+
+char i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 97 && str[i] <= 122)
+{
+str[i] = str[i] -32;
+}
+return (str);
+}

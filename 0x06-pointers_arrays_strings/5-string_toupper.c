c883bee10247e0d298a006bde80ccd62ae7dff11
farida <farida@gmail.com>
2023-07-28 12:05:34 -0700

    hsh

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,10 @@
+#include"main.h"
+
 /**
  * *string_toupper - changes lowercase letters to uppercase.
  *
  * @str: string to return
- * 
+ *
  * Return: str
  */
 
@@ -15,7 +17,7 @@ for (i = 0; str[i] != '\0'; i++)
 {
 if (str[i] >= 97 && str[i] <= 122)
 {
-str[i] = str[i] -32;
+str[i] = str[i] - 32;
 }
 return (str);
 }

741cee66cd30981c6a61c403db4012a9eb88fb65
farida <farida@gmail.com>
2023-07-31 16:08:26 -0700

    dasd

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * *_strchr - locates a character in a string.
+ *
+ * @s: char
+ * @c: second char
+ *
+ * Return: a pointer to c in the string s, or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+
+if (s[i] == c)
+{
+return (s);
+}
+return NULL;
+}

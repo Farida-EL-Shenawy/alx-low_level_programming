55bfd02d96d9da07ee7dc54bc21c495e576adeb2
farida <farida@gmail.com>
2023-08-10 12:45:02 -0700

    gshsj

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc.
+ *
+ * @b: unsigned int
+ *
+ * Return: pointer to the array initialized or null
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+int *m = malloc(b);
+
+if (m == 0)
+exit(98);
+
+return (m);
+}
